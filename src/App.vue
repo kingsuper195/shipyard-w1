37b44457f970f681d3b440498cb5dab7cb2e8576
Peter Durdin <peter.durdin.superworld@gmail.com>
2026-05-26 20:05:49 +0200

    Got basic time multiplication working.

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const pntData = ref([
+    {"hours":0, "multi":1},
+    {"hours":0, "multi":1},
+    {"hours":0, "multi":1},
+    {"hours":0, "multi":1},
+    {"hours":0, "multi":1},
+    {"hours":0, "multi":1},
+    {"hours":0, "multi":1},
+    {"hours":0, "multi":1},
+  ])
+
+</script>
+
+<template>
+  <table>
+      <tbody>
+      <tr>
+        <th>Week</th>
+        <th>Hours</th>
+        <th>Multiplier</th>
+        <th>Points</th>
+      </tr>
+      <tr>
+        <th>1</th>
+        <th class="w1 hours"><input v-model="pntData[1].hours"></th>
+        <th class="w1 multi"><input v-model="pntData[1].multi"></th>
+        <th class="w1 pnts">{{ (pntData[1].hours * 5) * pntData[1].multi + 35 }}</th>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
